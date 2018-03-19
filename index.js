2807c5c37064cc10987bd96448f1588ca13e415d
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 11:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getRepositories() {
   const req = new XMLHttpRequest()
   let username = document.getElementById('username')
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.open("GET", 'https://api.github.com/users/' + suername + '/repos')
   req.send()
 }
