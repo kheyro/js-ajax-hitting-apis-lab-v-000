c89897fe37a870c2f2a0be0e10f336ba7d5f160a
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 11:51:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getRepositories() {
   const req = new XMLHttpRequest()
   let username = document.getElementById('username').value
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", 'https://api.github.com/users/' + username + '/repos')
   req.send()
 }
