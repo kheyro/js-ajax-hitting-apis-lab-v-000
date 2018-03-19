4e47b93da0e8b8b407cc70ab86c635c6c99e67bc
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 11:52:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function displayRepositories() {
   let repoList = `<ul>${repo.map(repo => '<li><a href="' + repo.url + '">' + repo.name + '</a></li>' ).join('')}</ul>`
   document.getElementById('repositories').innerHTML = repoList
 }
+
+function displayCommits() {
+  let repo = JSON.parse(this.responseText)
+  console.log(repo)
+  let repoList = `<ul>${repo.map(repo => '<li><a href="' + repo.url + '">' + repo.name + '</a></li>' ).join('')}</ul>`
+  document.getElementById('repositories').innerHTML = repoList
+}
